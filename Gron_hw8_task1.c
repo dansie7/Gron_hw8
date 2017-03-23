b22e3b4e6befceaaa73b1bafadf6e75b9627fd12
Sharon Dansie <sharondansie@mail.weber.edu>
2017-03-23 16:17:58 +0000

    Initial commit

---
+++ Gron_hw8_task1.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Gron_hw8_task1.c
+ *       	Usage:  ./Gron_hw8_task1.c
+ *    Description:  Task1
+ *
+ *        Version:  1.0
+ *        Created:  03/23/2017 04:16:42 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Sharon Dansie (), sharondansie@weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
