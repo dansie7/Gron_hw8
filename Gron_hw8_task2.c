57ca25ae92f0657e1d586cd9d149a7afdd90cc20
ahlweb <adamlee1@mail.weber.edu>
2017-03-31 21:59:12 +0000

    first usage param

--- Gron_hw8_task2.c
+++ Gron_hw8_task2.c
@@ -8,17 +8,25 @@
  *        Version:  1.0
  *        Created:  03/23/2017 04:17:12 PM
  *       Compiler:  gcc -Wall -Werror
- *         Author:  Sharon Dansie (), sharondansie@weber.edu
+ *         Author:  Sharon Dansie (), Adam Lee  sharondansie@weber.edu
  * =====================================================================================
  */
 #include <stdio.h>		/* For Standard I/O */
 #include <stdlib.h>
 
+#define SIZE 2
 /* Function Prototypes */
+void Usage(char ar[]);
 
 /* Main Program */
 int main(int argc, char *argv[])
 {
+	char message[SIZE];
+	if (argc != 2)
+	{
+	printf("Missing or wrong numer of parameters\n");
+	Usage(message);
+	}
 
 	return 0;
 }
@@ -26,4 +34,13 @@ int main(int argc, char *argv[])
 
 /* Function Defenitions */
 
+void Usage(char ar[])
+{
+
+	printf("Usage: ./task2 [-p | -u | -l]\n");
+	
+	
+	return;
+
+}
 
