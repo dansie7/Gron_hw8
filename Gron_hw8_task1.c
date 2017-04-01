201ac8c4d9e3280cc8e32390715764bc662d9de4
Sharon Dansie <sharondansie@mail.weber.edu>
2017-04-01 16:13:49 +0000

    Added Usage function

--- Gron_hw8_task1.c
+++ Gron_hw8_task1.c
@@ -13,17 +13,38 @@
  */
 #include <stdio.h>		/* For Standard I/O */
 #include <stdlib.h>
+#include <string.h>
 
+#define SIZE 80
 /* Function Prototypes */
-
+void Usage(char ** info);
 /* Main Program */
 int main(int argc, char *argv[])
 {
-
 	return 0;
 }
 
 
 /* Function Defenitions */
+void Usage(char ** info)
+{
+	char str1[SIZE] == "--help";
+	if(strcmp(*info, str1) == 0 )
+	{
+		printf("Help Information\n");
+		printf("Usage ./task1 <str1> <str2>\n");
+		printf("Program checks if str2 is part of str1\n");
+		exit(1);
+	}
+	else
+	{
+		printf("Missing required parameters\n");
+		printf("Usage ./task1 <str1> <str2>\n");
+		printf("Program checks if str2 is part of str1\n");
+		exit(1);
+	}
+
+	return;
+}
 
 
